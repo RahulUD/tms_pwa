be121eb029c60a60b02da47ca36f1c753c2dfd54
rahul <rahul.adsps@gmail.com>
2020-12-05 10:54:26 -0800

    jjj

---
+++ dist/precache-manifest.8d421c7a9548eb5f15767566780429a0.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbe77566dedec6841f499c54fbbdb6c",
+    "url": "/bootstrap2052/album/album.css"
+  },
+  {
+    "revision": "6e3e7b8b92685e69da048493c4afda8b",
+    "url": "/bootstrap2052/album/index.html"
+  },
+  {
+    "revision": "987cc8795f6ae5adb1f0938614423163",
+    "url": "/bootstrap2052/assets/brand/bootstrap-outline.svg"
+  },
+  {
+    "revision": "16fa1cbd467464183c18257777207ef4",
+    "url": "/bootstrap2052/assets/brand/bootstrap-solid.svg"
+  },
+  {
+    "revision": "56190e233adeee9042990edaef6ab6dd",
+    "url": "/bootstrap2052/assets/dist/css/bootstrap-grid.css"
+  },
+  {
+    "revision": "669404687b63e2bb1d1830bdc0365d7e",
+    "url": "/bootstrap2052/assets/dist/css/bootstrap-grid.min.css"
+  },
+  {
+    "revision": "e5e6a51b28495e17e8ae63d2719d573c",
+    "url": "/bootstrap2052/assets/dist/css/bootstrap-reboot.css"
+  },
+  {
+    "revision": "78c67048b56f66e7702c7f718868f9d3",
+    "url": "/bootstrap2052/assets/dist/css/bootstrap-reboot.min.css"
+  },
+  {
+    "revision": "b2c06c42fd29dbb0b1205956752272e5",
+    "url": "/bootstrap2052/assets/dist/css/bootstrap.css"
+  },
+  {
+    "revision": "816af0eddd3b4822c2756227c7e7b7ee",
+    "url": "/bootstrap2052/assets/dist/css/bootstrap.min.css"
+  },
+  {
+    "revision": "f6f318002edd81c17fe079429cd8d866",
+    "url": "/bootstrap2052/assets/dist/js/bootstrap.bundle.js"
+  },
+  {
+    "revision": "21f815ff6d1883c4e81d821d38ff4070",
+    "url": "/bootstrap2052/assets/dist/js/bootstrap.bundle.min.js"
+  },
+  {
+    "revision": "54bdb9610fd9a27da4fddd99c39de9e5",
+    "url": "/bootstrap2052/assets/dist/js/bootstrap.js"
+  },
+  {
+    "revision": "02d223393e00c273efdcb1ade8f4f8b1",
+    "url": "/bootstrap2052/assets/dist/js/bootstrap.min.js"
+  },
+  {
+    "revision": "5e7130dde620054fa9f5d495192288f1",
+    "url": "/bootstrap2052/blog/blog.css"
+  },
+  {
+    "revision": "2f10f4d7cfe853d696ee4fa08d4132f8",
+    "url": "/bootstrap2052/blog/index.html"
+  },
+  {
+    "revision": "8ed670982d88a10d6e40f64c525b21a7",
+    "url": "/bootstrap2052/carousel/carousel.css"
+  },
+  {
+    "revision": "c6abeb7091fadce757e8413fd720d6c9",
+    "url": "/bootstrap2052/carousel/index.html"
+  },
+  {
+    "revision": "8e5fb15c4155f38020798e5c2bfb38b7",
+    "url": "/bootstrap2052/checkout/form-validation.css"
+  },
+  {
+    "revision": "1d363cb414aa6c2c96d12200f891d3b1",
+    "url": "/bootstrap2052/checkout/form-validation.js"
+  },
+  {
+    "revision": "623b41083c875cb09d7eb23e527777ae",
+    "url": "/bootstrap2052/checkout/index.html"
+  },
+  {
+    "revision": "211daf4eee015cb38fb10893c4645bbb",
+    "url": "/bootstrap2052/cover/cover.css"
+  },
+  {
+    "revision": "d817017d848d6a4fabbc939bcad64974",
+    "url": "/bootstrap2052/cover/index.html"
+  },
+  {
+    "revision": "1d232cb9fc4381efdb6326711f17786e",
+    "url": "/bootstrap2052/dashboard/dashboard.css"
+  },
+  {
+    "revision": "7700a591aced139da33c9d9a2ef0a2e7",
+    "url": "/bootstrap2052/dashboard/dashboard.js"
+  },
+  {
+    "revision": "3440c502e104e800b066387d02e8226c",
+    "url": "/bootstrap2052/dashboard/index.html"
+  },
+  {
+    "revision": "1767d44088ddea018f940e8b97be2777",
+    "url": "/bootstrap2052/floating-labels/floating-labels.css"
+  },
+  {
+    "revision": "1b29540dddb4a5446191cccf6617e2b5",
+    "url": "/bootstrap2052/floating-labels/index.html"
+  },
+  {
+    "revision": "5cc60afdb383a322205760f575b6250c",
+    "url": "/bootstrap2052/grid/grid.css"
+  },
+  {
+    "revision": "aa16bac17319d0af765067dd4ffde5fd",
+    "url": "/bootstrap2052/grid/index.html"
+  },
+  {
+    "revision": "117cdce3da63f50acf9d20a8ca11337c",
+    "url": "/bootstrap2052/jumbotron/index.html"
+  },
+  {
+    "revision": "0ef7edc6babea5a47645bda0c45368aa",
+    "url": "/bootstrap2052/jumbotron/jumbotron.css"
+  },
+  {
+    "revision": "ae908f07b315e342e37f37f141706a18",
+    "url": "/bootstrap2052/navbar-bottom/index.html"
+  },
+  {
+    "revision": "987183bd98a8d2cdf5a0dd43a3f34d85",
+    "url": "/bootstrap2052/navbar-fixed/index.html"
+  },
+  {
+    "revision": "3d46ddff119cfe2886a34b72aefd42a6",
+    "url": "/bootstrap2052/navbar-fixed/navbar-top-fixed.css"
+  },
+  {
+    "revision": "3c7c33fad92a664486acf8cc60f11915",
+    "url": "/bootstrap2052/navbar-static/index.html"
+  },
+  {
+    "revision": "ae704085e05c4bc6a705b225b03a5aea",
+    "url": "/bootstrap2052/navbar-static/navbar-top.css"
+  },
+  {
+    "revision": "06fa40df54c272fab815da37ce8463aa",
+    "url": "/bootstrap2052/navbars/index.html"
+  },
+  {
+    "revision": "f95ea8bb033949bba31b05925773e223",
+    "url": "/bootstrap2052/navbars/navbar.css"
+  },
+  {
+    "revision": "d8fc334be17d79fa0f53f38b2e2d0d61",
+    "url": "/bootstrap2052/offcanvas/index.html"
+  },
+  {
+    "revision": "8c0900c4071afea1c2789a250de3c22d",
+    "url": "/bootstrap2052/offcanvas/offcanvas.css"
+  },
+  {
+    "revision": "5c0edf2d5a4d88ec65e801c0f58ef23a",
+    "url": "/bootstrap2052/offcanvas/offcanvas.js"
+  },
+  {
+    "revision": "3d582396ff5a6f6e56449ff7bb22f665",
+    "url": "/bootstrap2052/pricing/index.html"
+  },
+  {
+    "revision": "c06793e3c3a28147e7efcec8c2636538",
+    "url": "/bootstrap2052/pricing/pricing.css"
+  },
+  {
+    "revision": "2889abd0fedb431a9fca115b059da3ae",
+    "url": "/bootstrap2052/product/index.html"
+  },
+  {
+    "revision": "e4efe420b0a96e18293c1afeb20ce6d3",
+    "url": "/bootstrap2052/product/product.css"
+  },
+  {
+    "revision": "5ae7f0e4f5e50f8b13371b00eda4a461",
+    "url": "/bootstrap2052/sign-in/index.html"
+  },
+  {
+    "revision": "00e205f17ee0cd3f59d5e1e1e9cf3b3f",
+    "url": "/bootstrap2052/sign-in/signin.css"
+  },
+  {
+    "revision": "497c5a5ea89bf99c1bf23c92063ab798",
+    "url": "/bootstrap2052/starter-template/index.html"
+  },
+  {
+    "revision": "8cb4aab3660723b641b6458f1a1d3ab1",
+    "url": "/bootstrap2052/starter-template/starter-template.css"
+  },
+  {
+    "revision": "323d4a6d1df7d985b5b2d40fbb55d1a3",
+    "url": "/bootstrap2052/sticky-footer-navbar/index.html"
+  },
+  {
+    "revision": "6fdbd6c044785ad21176f0a63e5ff5d2",
+    "url": "/bootstrap2052/sticky-footer-navbar/sticky-footer-navbar.css"
+  },
+  {
+    "revision": "fba80d22cf845905b9352c7351253c75",
+    "url": "/bootstrap2052/sticky-footer/index.html"
+  },
+  {
+    "revision": "08458f94e3c790fde385d34fb4c8612c",
+    "url": "/bootstrap2052/sticky-footer/sticky-footer.css"
+  },
+  {
+    "revision": "f26d7f446879fbaf3497",
+    "url": "/css/app.b38fe710.css"
+  },
+  {
+    "revision": "90b4f113e0fc5452e43c",
+    "url": "/css/chunk-269f9beb.d8608e93.css"
+  },
+  {
+    "revision": "0037e1f43eeb4885f9f2",
+    "url": "/css/chunk-vendors.2d065fb2.css"
+  },
+  {
+    "revision": "7cf1a277743b200c35ea7e521ab2f890",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f26d7f446879fbaf3497",
+    "url": "/js/app.fad3ac5a.js"
+  },
+  {
+    "revision": "90b4f113e0fc5452e43c",
+    "url": "/js/chunk-269f9beb.39d6b981.js"
+  },
+  {
+    "revision": "6034351404eef2f09c6b",
+    "url": "/js/chunk-2d0be6eb.8949cf7b.js"
+  },
+  {
+    "revision": "5fe326680ba109cd7023",
+    "url": "/js/chunk-2d230530.9239dcd9.js"
+  },
+  {
+    "revision": "0037e1f43eeb4885f9f2",
+    "url": "/js/chunk-vendors.bc77092c.js"
+  },
+  {
+    "revision": "fbc4e02c05da117d7ae8472bf602a2d4",
+    "url": "/loader.gif"
+  },
+  {
+    "revision": "5ba96c6300e5a3a7f4c4d7d6fe18f526",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
